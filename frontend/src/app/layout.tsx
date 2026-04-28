d97556b18ecf96dc1bd656da4190ef83ad3bd1c9
tony <tmtony98@gmail.com>
2026-04-28 12:38:20 +0530

    feat: add Google Tag Manager script to RootLayout for enhanced tracking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { CurrencyProvider } from "@/context/CurrencyContext";
@@ -35,6 +36,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KW4SF2MH');`}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
@@ -47,8 +57,8 @@ export default function RootLayout({
             </CurrencyProvider>
           </AuthProvider>
         </GoogleOAuthWrapper>
+        <GoogleAnalytics gaId="G-R9LDDE3P0X" />
       </body>
-      <GoogleAnalytics gaId="G-R9LDDE3P0X" />
     </html>
   );
 }
